f77c2adc7627c8902182f9873ce917b1d8d9745b
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-10-31 15:49:29 +0100

    Update App.js

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import Counter from './Counter';
+
+import {
+  Route,
+  Switch,
+  // Redirect,
+  // useRouteMatch,
+} from 'react-router-dom';
+
+// == styles
+import './reset.css';
+import './App.css';
+
+// == component
+import NavBar from './NavBar';
+import Login from './Login';
+import Signup from './Signup';
 
 const App = () => (
   <div className="App">
-    APP
-    <Counter />
-    <p>blabla</p>
+    <NavBar />
+    <Switch>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+    </Switch>
   </div>
 );
 
