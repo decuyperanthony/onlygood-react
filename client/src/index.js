22a88e44e2f2abdfcc272f6689f724df3efcc4e2
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-10-31 15:50:04 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // == redux
 import { Provider } from 'react-redux';
+// eslint-disable-next-line import/extensions
 import store from './store/index.js';
 
-
 import App from './App/App';
+import Loading from './App/Loading';
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Provider store={store}>
+      <Suspense fallback={(<Loading />)}>
+        <App />
+      </Suspense>
+    </Provider>
+  </Router>, document.getElementById('root'),
+);
