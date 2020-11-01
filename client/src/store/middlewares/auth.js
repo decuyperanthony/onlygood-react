9d995b5083f95ace5cb31e6ee4f89cff5c333ad8
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-01 16:12:36 +0100

    Update auth.js

--- client/src/store/middlewares/auth.js
+++ client/src/store/middlewares/auth.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-console */
-import React from 'react';
+// import React from 'react';
 import axios from 'axios';
 // import { useTranslation } from 'react-i18next';
 
 import { LOGIN } from '../action/auth';
+import { enterHomePage } from '../action/route';
 
 // import { setLoaderOpen, setLoaderClose } from '../../actions/loader';
 
@@ -31,73 +32,20 @@ export default (store) => (next) => (action) => {
         .then((res) => {
           console.log('>>>>>>> >>>>>> res.data', res.data);
           if (res.status === 200) {
-            console.log('res', res);
+            console.log('res', res.data);
             // console.log('fneled');
             // console.log('res', res.data.user.mail_actived);
             // if (res.data.user.mail_actived) {
             //   store.dispatch(setStep(1));
             // }
-            // requete pour savoir si le user a rempli ses détails
-            // console.log('res.data.userDetails', res.data.userDetails);
-            // console.log('res.data.userList', res.data.userList);
-            // localStorage.valueSocialNewtorkTab = JSON.stringify(1);
-            // localStorage.user = JSON.stringify(res.data.user);
+            localStorage.user = JSON.stringify(res.data.user);
             // localStorage.userToken = JSON.stringify(res.data.userToken);
-            // localStorage.userDetails = JSON.stringify(res.data.userList.userDetails);
-            // localStorage.userSocialNetwork = JSON.stringify(res.data.userList.userSocialNetwork);
-            // localStorage.userPhone = JSON.stringify(res.data.userList.userPhone);
-
-            // if (res.data.userList.userDetails) {
-            //   // setStep(1)
-            //   store.dispatch(setStep(2));
-            // }
-
-            // if (res.data.userList.userSocialNetwork) {
-            //   store.dispatch(setStep(3));
-            // }
 
             // localStorage.userDetails = JSON.stringify(res.data.userDetails);
             // const user = JSON.parse(localStorage.getItem('user'));
-            // if (res.data.userList.userSocialNetwork) {
-            //   axios.get(`${API_URL}/getuseryoutubedata/user/${res.data.user.id}`)
-            //     .then((response) => {
-            //       localStorage.youtubeData = JSON.stringify(response.data);
-            //     });
-            //   //* == requete pour avoir les derniers posts du user
-            //   axios
-            //     .get(`${API_URL}/getlasttenpostsinfo/${res.data.user.id}`)
-            //     .then(async (resApi) => {
-            //       await console.log('resApi ==> ', resApi.data.listPosts);
-            //       localStorage.instagramPosts = JSON.stringify(resApi.data.listPosts);
-            //       await store.dispatch(setUserPostsInstagram(resApi.data.listPosts));
-            //       // if (resApi.data)
-            //       // resApi.data.listPosts.map((post) => {
-            //       //   console.log('post', post);
-            //       // });
-            //     })
-            //     .catch((err) => console.trace(err));
-            //   //* == requete pour avoir les infos insta du user
-            //   axios.get(`${API_URL}/getuserinfo/${res.data.user.id}`)
-            //     .then(async (resBackend) => {
-            //       // console.log('resBackend', resBackend);
-            //       // ? voir INSOMNIA DERNIERE REQUETE ET METTRE LE CODE ICI
-            //       // ? if (res.XXX.user_fb_access_token) blabla else ce qui suit
-            // eslint-disable-next-line max-len
-            //       // ? blabla = stocker dans le local storage le fait qu il n ait pas donné de token
-            //       //
-            //       localStorage.fbData = JSON.stringify(resBackend.data);
-            //       await store.dispatch(enterDashboardPage(action.payload.history));
-            //       store.dispatch(setLoaderClose());
-            //     })
-            //     .catch((err) => {
-            //       store.dispatch(setLoaderClose());
-            //       console.log('on est dans le catch');
-            //       console.trace(err);
-            //     });
-            // }
             // store.dispatch(setUser(user));
             // store.dispatch(setLoaderClose());
-            //! store.dispatch(enterDashboardPage(action.payload.history));
+            store.dispatch(enterHomePage(action.payload.history));
           }
         })
         .catch((error) => {
