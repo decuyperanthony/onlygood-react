d8863089fee04311c499debf6814f320abde8a55
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-01 16:12:27 +0100

    Update route.js

--- client/src/store/action/route.js
+++ client/src/store/action/route.js
@@ -1,3 +1,10 @@
 export const ENTER_LOGIN_PAGE = 'actions/ENTER_LOGIN_PAGE';
-export const ENTER_HOME_PAGE = 'actions/ENTER_HOME_PAGE';
 export const ENTER_ADMIN_PAGE = 'actions/ENTER_ADMIN_PAGE';
+
+// ===  ENTER_HOME_PAGE
+export const ENTER_HOME_PAGE = 'actions/ENTER_HOME_PAGE';
+
+export const enterHomePage = (history) => ({
+  type: ENTER_HOME_PAGE,
+  history,
+});
