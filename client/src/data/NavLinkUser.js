c6ae6939f495cc8c060113f797c290424e66ae7a
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-07 00:27:22 +0100

    Create NavLinkUser.js

---
+++ client/src/data/NavLinkUser.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    route: '/dashboard',
+    label: 'Home',
+  },
+  {
+    route: '/explore',
+    label: 'Explore',
+  },
+  {
+    route: '/bookmarks',
+    label: 'Bookmarks',
+  },
+  // {
+  //   route: '/teachers',
+  //   label: 'Professeurs',
+  // },
+  // {
+  //   route: '/profiluser',
+  //   label: 'Mon compte',
+  // },
+];
