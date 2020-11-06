370e473cad8dcfe2b659b247f486ee30e2b60d0c
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-06 21:41:27 +0100

    Update App.js
    add dashboard in app and new route for

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -9,9 +9,12 @@ import Paper from '@material-ui/core/Paper';
 import {
   Route,
   Switch,
-  // Redirect,
-  // useRouteMatch,
+  Redirect,
+  useRouteMatch,
 } from 'react-router-dom';
+
+// === method
+import getUserData from '../utils/getUserData';
 
 // == styles
 // import './reset.css';
@@ -22,11 +25,87 @@ import AppBar from './NavBar/AppBar';
 // import NavBar from './NavBar';
 import Login from './Login';
 import Signup from './Signup';
+import Home from './Home';
+import Loader from './LoaderBackDrop';
+
+//* === === === === === === DASHBOARD === === === === === === === === === ===
+// == sur la route /dashboard/quelquechose
+const Dashboard = () => {
+  // const { articles } = useSelector((state) => state.shop);
+  // const dispatch = useDispatch();
+  // const classes = useStyles();
+  const { path } = useRouteMatch();
+  const user = JSON.parse(localStorage.getItem('user'));
+  const userToken = JSON.parse(localStorage.getItem('userToken'));
+
+  // == auto connect
+  if (userToken) {
+    const myId = user.id;
+    getUserData(myId);
+  }
+
+  // == stepper
+
+  return (
+    <>
+      <Loader />
+      <AppBar />
+      <Switch>
+        <Route
+          exact
+          path={`${path}/`}
+          render={(props) => {
+            if (!userToken) {
+              return <Redirect to="/login" />;
+            }
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            return <Home {...props} />;
+          }}
+        />
+        {/* {ArticleRouterJSX} */}
+
+        {/* {articles.map((article) => (
+          <Route
+            key={article.id}
+            exact
+            path={`${path}/${article.id}`}
+            render={(props) => {
+              if (!userToken) {
+                return <Redirect to="/login" />;
+              }
+              if (!userDetails || !userSocialNetwork || !userPhone) {
+                return <Redirect to={`${path}/registerstepper/`} />;
+              }
+              if (article) {
+                return <Article article={article} />;
+              }
+              return <Article {...props} />;
+            }}
+          />
+        ))} */}
+        {/* <Route><QuatreDashboard /></Route> */}
+      </Switch>
+    </>
+  );
+};
 
 const App = () => {
   // const [darkmode, setDarkmode] = useState(false);
   const { darkmodeBoolean } = useSelector((state) => state.theme);
+  const user = JSON.parse(localStorage.getItem('user'));
+  const userToken = JSON.parse(localStorage.getItem('userToken'));
+  // == auto connect
+  if (userToken) {
+    const myId = user.id;
+    getUserData(myId);
+    // getUserDataDetailsInterests(myId);
+    // getOrdersByUserId(myId);
+    // getUserInstagramData(myId);
+  }
+  //! manque la redirection apres le logout
+  //! vers la page login
 
+  // === theme
   const theme = createMuiTheme({
     palette: {
       primary: {
@@ -41,6 +120,8 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <Paper style={{ height: '100vh' }}>
         <div className="App">
+          <Loader />
+          {/* <AppBar /> */}
           <Switch>
             <Route exact path="/login">
               <Login />
@@ -48,7 +129,9 @@ const App = () => {
             <Route exact path="/signup">
               <Signup />
             </Route>
-            <AppBar />
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
           </Switch>
         </div>
       </Paper>
