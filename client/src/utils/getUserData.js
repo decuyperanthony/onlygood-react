d0b290e0813bf527e481af0b986ae582bed403eb
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-06 21:41:41 +0100

    Create getUserData.js

---
+++ client/src/utils/getUserData.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+// eslint-disable-next-line import/no-cycle
+import store from '../store/index';
+import { API_URL } from './constante';
+
+import { setUser } from '../store/action/auth';
+
+const getUserData = (userId) => {
+  console.log('on set le user data');
+  axios
+    .get(`${API_URL}/user/${userId}`)
+    .then((res) => {
+      store.dispatch(setUser(res.data));
+    })
+    // eslint-disable-next-line no-console
+    .catch((err) => console.trace(err));
+};
+
+export default getUserData;
