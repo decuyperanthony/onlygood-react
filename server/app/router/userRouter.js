2f53f21c1095e2fe84efb6e80f6d5ce7adaeca4f
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-07 00:27:41 +0100

    Create userRouter.js

---
+++ server/app/router/userRouter.js
@@ -0,0 +1,19 @@
+// == Require express
+const express = require('express');
+
+// == controller
+const userController = require('../controllers/userController');
+
+const router = express.Router();
+
+//* ----- ROUTE DES USERS -----
+router.get(`/${process.env.API_URL}/users`, userController.getAllUsers);
+router.get(`/${process.env.API_URL}/user/:id`, userController.getOneUser);
+// router.get(`/${process.env.API_URL}/usersdetails`, userController.getAllUsersDetails);
+// router.get(`/${process.env.API_URL}/userdetails/:id`, userController.getOneUserDetails);
+// router.get(`/${process.env.API_URL}/userdetail/:id`, userController.getOneUserDetail);
+// router.patch(`/${process.env.API_URL}/user/:id`, userController.updateUser);
+// router.delete(`/${process.env.API_URL}/user/:id`, userController.deleteUser);
+
+
+module.exports = router;
