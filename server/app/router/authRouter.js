22d5f042a4d9499d55551a07af54fedb2ad34beb
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-07 00:27:36 +0100

    Create authRouter.js

---
+++ server/app/router/authRouter.js
@@ -0,0 +1,15 @@
+// == Require express
+const express = require('express');
+
+// controller
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.post("/login", authController.loginAction);
+
+// page signup
+router.get('/signup', authController.signupPage);
+router.post("/signup", authController.signupAction);
+
+module.exports = router;
