62335f9bf946c04b7feb6575433afa82324e22b6
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-07 00:27:24 +0100

    Create navLinkVisitor.js

---
+++ client/src/data/navLinkVisitor.js
@@ -0,0 +1,14 @@
+export default [
+    {
+      route: '/',
+      label: 'Accueil',
+    },
+    {
+      route: '/login',
+      label: 'Connexion',
+    },
+    {
+      route: '/signup',
+      label: `S'inscrire`,
+    },
+  ];
