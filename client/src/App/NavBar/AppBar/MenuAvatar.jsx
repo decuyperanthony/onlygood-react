705519cec4ff9c70f86adf99a0ef166a42d78e4e
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-07 01:49:07 +0100

    Create MenuAvatar.jsx

---
+++ client/src/App/NavBar/AppBar/MenuAvatar.jsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import IconButton from '@material-ui/core/IconButton';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import { makeStyles } from '@material-ui/core/styles';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import BuildIcon from '@material-ui/icons/Build';
+
+import { useHistory } from 'react-router-dom';
+
+import PopConfirm from './PopConfirm';
+
+const firstPath = 'dashboard';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    // marginRight: '-2em',
+    // marginLeft: '-1em',
+  },
+  paper: {
+    // marginRight: theme.spacing(2),
+    // [theme.breakpoints.up('xs')]: {
+    //   marginRight: '2em',
+    // },
+  },
+}));
+
+export default function MenuAvatar() {
+  const history = useHistory();
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const handleProfileClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+    history.push(`/${firstPath}/profile`);
+    setOpen(false);
+  };
+  // const handleAccountClose = (event) => {
+  //   if (anchorRef.current && anchorRef.current.contains(event.target)) {
+  //     return;
+  //   }
+  //   history.push(`/${firstPath}/social`);
+  //   setOpen(false);
+  // };
+  const handleParametersClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+    history.push(`/${firstPath}/profile/parameters`);
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  return (
+    <div className={classes.root}>
+      <div>
+        <Button
+          ref={anchorRef}
+          aria-controls={open ? 'menu-list-grow' : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+        >
+          <ExpandMoreIcon style={{ color: '#263153' }} />
+        </Button>
+        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...TransitionProps}
+              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList style={{ marginTop: '1em' }} autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                    <MenuItem onClick={handleProfileClose}>
+                      <IconButton
+                        aria-label="account of current user"
+                        aria-controls="primary-search-account-menu"
+                        aria-haspopup="true"
+                        color="inherit"
+                      >
+                        <AccountCircle style={{ color: 'black' }} />
+                      </IconButton>
+                      Profile
+                    </MenuItem>
+                    {/* <MenuItem onClick={handleAccountClose}>
+                      <IconButton
+                        aria-label="account of current user"
+                        aria-controls="primary-search-account-menu"
+                        aria-haspopup="true"
+                        color="inherit"
+                      >
+                        <AccountTreeIcon style={{ color: 'black' }} />
+                      </IconButton>
+                      {t('navbar-avatar-menu-myAccount')}
+                    </MenuItem> */}
+                    <MenuItem onClick={handleParametersClose}>
+                      <IconButton
+                        aria-label="account of current user"
+                        aria-controls="primary-search-account-menu"
+                        aria-haspopup="true"
+                        color="inherit"
+                      >
+                        <BuildIcon style={{ color: 'black' }} />
+                      </IconButton>
+                      Parameters
+                    </MenuItem>
+                    <PopConfirm />
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </div>
+    </div>
+  );
+}
