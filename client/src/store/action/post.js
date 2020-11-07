8dce5a727487fdb3bb9f49a73626cf9c1ac4f410
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-07 20:44:52 +0100

    Create post.js
    action

---
+++ client/src/store/action/post.js
@@ -0,0 +1,15 @@
+// ===  SET_ALL_POSTS
+export const SET_ALL_POSTS = 'SET_ALL_POSTS';
+
+export const setAllPosts = (payload) => ({
+  type: SET_ALL_POSTS,
+  payload,
+});
+
+// ===  ADD_POST
+export const ADD_POST = 'ADD_POST';
+
+export const addPost = (payload) => ({
+  type: ADD_POST,
+  payload,
+});
