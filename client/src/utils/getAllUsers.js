fb8a91a623cf5c31ad1a6e28cf3afd2cdcced6c3
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-08 18:50:40 +0100

    Create getAllUsers.js

---
+++ client/src/utils/getAllUsers.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+// eslint-disable-next-line import/no-cycle
+import store from '../store/index';
+
+import { API_URL } from './constante';
+
+// == actions
+import { SET_ALL_USERS } from '../store/action/user';
+
+const getAllUsers = (url = `${API_URL}/users`) => {
+  const promise = axios.get(
+    url,
+  );
+  promise.then((res) => {
+    console.log('res.data', res.data);
+    const users = res.data;
+    store.dispatch({ type: SET_ALL_USERS, payload: users });
+  });
+};
+
+export default getAllUsers;
