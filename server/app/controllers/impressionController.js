40cba6ca10317f5fc1fcee87863d3f038540adc1
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-08 17:11:37 +0100

    comments ok! can add or remove comment
    - can also have post with all comments

--- server/app/controllers/impressionController.js
+++ server/app/controllers/impressionController.js
@@ -1,6 +1,7 @@
 const { User_likes_post } = require('../models');
 const { User_saved_post } = require('../models');
 const { User_retweet_post } = require('../models');
+const { User_comments_post } = require('../models');
 
 const impressionController = {
     //* ----------- LIKES -------------
@@ -68,7 +69,7 @@ const impressionController = {
         }
     },
 
-    //* ----------- SAVED -------------
+    //* ----------- RETWEET -------------
     addOrRemoveRetweetToPost: async (req, res) => {
         // console.log('add like');
         try {
@@ -93,6 +94,68 @@ const impressionController = {
                 const newRetweet = new User_retweet_post(req.body);
                 const savedRetweet = await newRetweet.save();
                 res.status(200).send(savedRetweet);
+            }
+
+        } catch (error) {
+            console.trace(error);
+            res.status(500).send(error);
+        }
+    },
+
+    //* ----------- COMMENT -------------
+    getAllComments: async (req, res) => {
+        try {
+            const comments = await User_comments_post.findAll();
+            res.send(comments);
+        } catch (error) {
+            console.trace(error);
+            res.status(500).send(error);
+        }
+    },
+
+    getCommentByPostId: async (req, res) => {
+        try {
+            const comment = await User_comments_post.findOne({
+                where: {
+                    post_id: req.body.post_id
+                }
+            });
+            if (!comment) {
+                res.send('ce post ne possède pas de commentaires')
+            } else {
+                res.send(comment);
+            }
+
+        } catch (error) {
+            console.trace(error);
+            res.status(500).send(error);
+        }
+    },
+
+    addOrRemoveCommentToPost: async (req, res) => {
+        // console.log('add like');
+        try {
+            // console.log('req.body', req.body);
+            // const app_users_id = req.body.app_users_id;
+            //! on check si la relation existe
+            const comment = await User_comments_post.findOne({
+                where: {
+                    app_users_id: req.body.app_users_id,
+                    post_id: req.body.post_id
+                }
+            });
+
+            if (comment) {
+                //si la relation existe on supprime
+                // console.log('ok il a deja like il faut supprimer');
+                comment.destroy();
+                res.status(200).send('commentaire sur le post supprimée');
+            } else {
+                // si elle n'existe pas on la crée
+                // console.log('il a jamais liké ce post alors on ajoute la ligne');
+                const newComment = new User_comments_post(req.body);
+                const savedComment = await newComment.save();
+                res.status(200).send(savedComment);
             }
 
         } catch (error) {
