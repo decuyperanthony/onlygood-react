7ae735d23f3ced7ce4ba8e2236a8aacd689cfa9a
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-08 18:49:48 +0100

    Update Avatar.js

--- client/src/App/Explore/Avatar.js
+++ client/src/App/Explore/Avatar.js
@@ -1,8 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
-import avatarAntho from '../../image/avatar.jpg';
+// import avatarAntho from '../../image/avatar.jpg';
+import { API_URL } from '../../utils/constante';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,7 +20,8 @@ export default function ImageAvatars({ pictureSrc }) {
 
   return (
     <div className={classes.root}>
-      <Avatar alt="Antho Sharp" src={avatarAntho} />
+      {/* <Avatar alt="Antho Sharp" src=`{pictureSrc}` /> */}
+      <Avatar alt="Antho Sharp" src={`${API_URL}/img/${pictureSrc}`} />
     </div>
   );
 }
