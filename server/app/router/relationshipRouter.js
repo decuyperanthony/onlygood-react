6daa31e73422f93c40d24a8d953b303153e04937
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-09 01:33:16 +0100

    Update relationshipRouter.js

--- server/app/router/relationshipRouter.js
+++ server/app/router/relationshipRouter.js
@@ -9,9 +9,9 @@ const router = express.Router();
 //* ----- ROUTE DES RELATION FOLLOWER FOLLOWED -----
 router.get(`/relationships`, relationshipController.getAllRelationships);
 router.get(`/relationship/:id`, relationshipController.getOneRelationship);
-router.post(`/relationship`, relationshipController.addRelationship);
+router.post(`/relationship`, relationshipController.addOrRemoveRelationship);
 // router.patch(`/relationship/:id`, postController.updatePost);
-router.delete(`/relationship/:id`, relationshipController.removeRelationship);
+// router.delete(`/relationship/:id`, relationshipController.removeRelationship);
 
 
 module.exports = router;
