32003894c48612dd3724c6a7b4f9fa58745b5489
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-09 13:29:11 +0100

    Update user.js

--- client/src/store/reducers/user.js
+++ client/src/store/reducers/user.js
@@ -1,9 +1,11 @@
 import {
   SET_ALL_USERS,
+  SET_ONE_USER_BY_ID,
 } from '../action/user';
 
 export const initialState = {
   users: [],
+  user: [],
 };
 
 const user = (state = initialState, action = {}) => {
@@ -12,6 +14,12 @@ const user = (state = initialState, action = {}) => {
       return {
         ...state,
         users: action.payload,
+        // message: action.message,
+      };
+    case SET_ONE_USER_BY_ID:
+      return {
+        ...state,
+        user: action.payload,
         // message: action.message,
       };
 
