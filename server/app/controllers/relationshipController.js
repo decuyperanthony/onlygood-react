bdda32592d8e5704e04708356c55b2ab2aaea0c4
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-09 01:33:13 +0100

    Update relationshipController.js
    add toggle method for followers followed

--- server/app/controllers/relationshipController.js
+++ server/app/controllers/relationshipController.js
@@ -44,10 +44,30 @@ const relationshipController = {
         }
     },
 
-    addRelationship: async (req, res) => {
+    addOrRemoveRelationship: async (req, res) => {
         console.log('add relation')
         try {
             console.log('req.body', req.body);
+            //! on check si la relation existe
+            const relation = await Relationship.findOne({
+                where: {
+                    followed_id: req.body.followed_id,
+                    follower_id: req.body.follower_id
+                }
+            });
+            if (relation) {
+                //si la relation existe on supprime
+                console.log('ok il le suit deja il faut supprimer');
+                relation.destroy();
+                res.status(200).send('relation supprimée');
+            } else {
+                // si elle n'existe pas on la crée
+                // console.log('il a jamais liké ce post alors on ajoute la ligne');
+                const newRelation = new Relationship(req.body);
+                const savedRelation = await newRelation.save();
+                res.status(200).send(savedRelation);
+            }
+
             // const { title } = req.body;
             // const findBrand = await Brand.findOne({
             //     where: {
@@ -57,9 +77,9 @@ const relationshipController = {
             // if (findBrand) {
             //     res.send('cet marque existe déja');
             // } else {
-                const newRelation = new Relationship(req.body);
-                const savedRelation = await newRelation.save();
-                res.status(200).send(savedRelation);
+                // const newRelation = new Relationship(req.body);
+                // const savedRelation = await newRelation.save();
+                // res.status(200).send(savedRelation);
             // }
 
         } catch (error) {
@@ -83,20 +103,20 @@ const relationshipController = {
     //     }
     // },
 
-    removeRelationship: async (req, res, next) => {
-        const relationId = req.params.id;
-        try {
-            let relation = await Relationship.findByPk(relationId);
-            if (!relation) {
-                return res.status(401).send('Cet relation n\'existe pas');
-            }
-            relation.destroy();
-            res.status(200).send('relation supprimée');
-        } catch (error) {
-            console.trace(error);
-            res.status(500).send(error);
-        }
-    }
+    // removeRelationship: async (req, res, next) => {
+    //     const relationId = req.params.id;
+    //     try {
+    //         let relation = await Relationship.findByPk(relationId);
+    //         if (!relation) {
+    //             return res.status(401).send('Cet relation n\'existe pas');
+    //         }
+    //         relation.destroy();
+    //         res.status(200).send('relation supprimée');
+    //     } catch (error) {
+    //         console.trace(error);
+    //         res.status(500).send(error);
+    //     }
+    // }
 }
 
 module.exports = relationshipController;
