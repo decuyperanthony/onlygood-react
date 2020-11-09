a8e57832c6d48005e8e074d167a19e87ec673867
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-09 13:29:14 +0100

    Create getUserById.js

---
+++ client/src/utils/getUserById.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+// eslint-disable-next-line import/no-cycle
+import store from '../store/index';
+
+import { API_URL } from './constante';
+
+// == actions
+import { SET_ONE_USER_BY_ID } from '../store/action/user';
+//! changer la route
+
+const getUserById = (userId) => {
+  const promise = axios.get(`${API_URL}/user/${userId}`);
+  promise.then((res) => {
+    const user = res.data;
+    // console.log('orders', orders);
+    store.dispatch({ type: SET_ONE_USER_BY_ID, payload: user });
+  });
+};
+
+export default getUserById;
