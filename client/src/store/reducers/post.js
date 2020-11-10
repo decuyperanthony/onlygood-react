d92513155572998edad8afa7fca67fb1bf82fc64
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-10 11:16:21 +0100

    Update post.js
    reducer

--- client/src/store/reducers/post.js
+++ client/src/store/reducers/post.js
@@ -1,9 +1,11 @@
 import {
   SET_ALL_POSTS,
+  SET_WHICH_TWEET,
 } from '../action/post';
 
 export const initialState = {
   posts: [],
+  filteredProfilePosts: 'Tweet',
 };
 
 const post = (state = initialState, action = {}) => {
@@ -13,6 +15,15 @@ const post = (state = initialState, action = {}) => {
         ...state,
         posts: action.payload,
         // message: action.message,
+      };
+
+    case SET_WHICH_TWEET:
+      console.log('SET_WHICH_TWEET');
+      console.log('action.payload', action.payload);
+      return {
+        ...state,
+        filteredProfilePosts: action.payload,
+        // posts: action.payload,
       };
 
     default:
