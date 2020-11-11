807f4be7aef4e8da4e36445fc09de70486c94bf9
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-11 16:42:49 +0100

    Update userRouter.js

--- server/app/router/userRouter.js
+++ server/app/router/userRouter.js
@@ -6,13 +6,16 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
+// multer
+const upload = require('../middleware/multer');
+
 //* ----- ROUTE DES USERS -----
 router.get(`/users`, userController.getAllUsers);
 router.get(`/user/:id`, userController.getOneUser);
 // router.get(`/${process.env.API_URL}/usersdetails`, userController.getAllUsersDetails);
 // router.get(`/${process.env.API_URL}/userdetails/:id`, userController.getOneUserDetails);
 // router.get(`/${process.env.API_URL}/userdetail/:id`, userController.getOneUserDetail);
-router.patch(`/user/:id`, userController.updateUser);
+router.patch(`/user/:id`, upload.single("image"), userController.updateUser);
 // router.delete(`/${process.env.API_URL}/user/:id`, userController.deleteUser);
 
 
