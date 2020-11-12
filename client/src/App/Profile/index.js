a3d0dfabc425399213728344831cf431b6bcca86
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-12 20:52:39 +0100

    Update index.js

--- client/src/App/Profile/index.js
+++ client/src/App/Profile/index.js
@@ -19,27 +19,40 @@ import { API_URL } from '../../utils/constante';
 import VerticalTabs from './VerticalTabs';
 import PostProfile from './Post';
 import ModalUpdateProfil from './ModalUpdateProfil';
+import ModalUpdateProfilMobile from './ModalUpdateProfilMobile';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+
     // padding: '1em',
   },
   paper: {
     padding: theme.spacing(2),
+    [theme.breakpoints.down('sm')]: {
+      paddingBottom: 0,
+    },
 
     // textAlign: 'center',
     color: theme.palette.text.secondary,
   },
   // card: {
   //   display: 'flex',
-  //   [theme.breakpoints.down('xs')]: {
-  //     width: '250px',
-  //   },
+
   // },
   containerProfilePage: {
-    maxWidth: '1000px',
+    maxWidth: '960px',
     margin: 'auto',
+    [theme.breakpoints.down('sm')]: {
+      // width: '250px',
+      margin: '0',
+      width: 'auto',
+    },
+  },
+  dispayNoneDownXS: {
+    [theme.breakpoints.down('xs')]: {
+      display: 'none',
+    },
   },
   // pictureHeader: {
 
@@ -50,6 +63,22 @@ const useStyles = makeStyles((theme) => ({
     border: '2px solid white',
     marginBottom: '-5em',
     marginLeft: '2em',
+    [theme.breakpoints.down('sm')]: {
+      // width: '250px',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      marginTop: '-7em',
+    },
+  },
+  blocUserInformation: {
+    marginLeft: '12em',
+    [theme.breakpoints.down('sm')]: {
+      // width: '250px',
+      marginLeft: '0',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
   },
   userInformation: {
     display: 'flex',
@@ -63,12 +92,20 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: '600',
     fontSize: '24px',
     marginRight: '1em',
+    [theme.breakpoints.down('sm')]: {
+      display: 'block',
+      marginRight: '0',
+    },
   },
   typoFollow: {
     fontFamily: 'popins, sans-serif',
     fontWeight: '500',
     fontSize: '12px',
     marginRight: '1em',
+    [theme.breakpoints.down('sm')]: {
+      // display: 'block',
+      marginRight: '0',
+    },
   },
   typoNumberOfFollow: {
     fontFamily: 'popins, sans-serif',
@@ -76,7 +113,7 @@ const useStyles = makeStyles((theme) => ({
     fontSize: '12px',
     marginRight: '1em',
   },
-  button: {
+  buttonDesktop: {
     color: 'white',
     borderRadius: '4px',
     fontFamily: 'Noto Sans, sans-serif',
@@ -87,6 +124,9 @@ const useStyles = makeStyles((theme) => ({
     '&:hover': {
       cursor: 'pointer',
     },
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
   },
   userDescription: {
     // display: 'flex',
@@ -96,6 +136,20 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: '500',
     fontSize: '18px',
     // marginRight: '1em',
+  },
+  containerInfoUser: {
+    [theme.breakpoints.down('sm')]: {
+      // // width: '250px',
+      // marginLeft: 'auto',
+      // marginRight: 'auto',
+      marginTop: '-0.5em',
+    },
+  },
+  containerFollowFollower: {
+    [theme.breakpoints.down('sm')]: {
+      dislay: 'flex',
+      justifyContent: 'center',
+    },
   },
   paperVerticalTabs: {
     padding: 0,
@@ -131,9 +185,9 @@ const Profile = () => {
             src={`${API_URL}/img/${user.picture_road}`}
             className={classes.avatar}
           />
-          <Grid item xs={12}>
+          <Grid item xs={12} className={classes.containerInfoUser}>
             <Paper className={classes.paper}>
-              <div style={{ marginLeft: '12em' }}>
+              <div className={classes.blocUserInformation}>
                 <div className={classes.userInformation}>
                   <div>
                     <span className={classes.typoName}>
@@ -141,34 +195,39 @@ const Profile = () => {
                       {' '}
                       {user.lastname}
                     </span>
-                    <span className={classes.typoFollow}>
-                      {/* <span>{user.followed.length}</span> */}
-                      {numberOfFollowers}
-                      {' '}
-                      <span className={classes.typoNumberOfFollow}>
-                        Following
+                    <div className={classes.containerFollowFollower}>
+                      <span className={classes.typoFollow}>
+                        {/* <span>{user.followed.length}</span> */}
+                        {numberOfFollowers}
+                        {' '}
+                        <span className={classes.typoNumberOfFollow}>
+                          Following
+                        </span>
                       </span>
-                    </span>
-                    <span className={classes.typoFollow}>
-                      {/* <span>{user.follower.length}</span> */}
-                      {numberOfFollows}
-                      {' '}
-                      <span className={classes.typoNumberOfFollow}> Followers</span>
-                    </span>
+                      <span className={classes.typoFollow}>
+                        {/* <span>{user.follower.length}</span> */}
+                        {numberOfFollows}
+                        {' '}
+                        <span className={classes.typoNumberOfFollow}>
+                          Followers
+                        </span>
+                      </span>
+                    </div>
                   </div>
                   <ModalUpdateProfil />
                   {/* <div className={classes.button}>Edit your profile</div> */}
                 </div>
                 <div className={classes.userDescription}>{user.description}</div>
+                <ModalUpdateProfilMobile />
               </div>
             </Paper>
           </Grid>
-          <Grid item xs={3}>
+          <Grid item xs={12} md={3}>
             <Paper className={classes.paperVerticalTabs}>
               <VerticalTabs />
             </Paper>
           </Grid>
-          <Grid item xs={9}>
+          <Grid item xs={12} md={9}>
             {/* <Paper className={classes.paper}> */}
             <PostProfile />
             {/* </Paper> */}
