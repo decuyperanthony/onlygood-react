66acd5205a8ff8bc698b529b819711e23dd74f0c
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-14 18:36:46 +0100

    delete picture's post from server when deleting a post done

--- server/app/router/postRouter.js
+++ server/app/router/postRouter.js
@@ -6,10 +6,13 @@ const postController = require('../controllers/postController');
 
 const router = express.Router();
 
+// multer
+const upload = require('../middleware/multer');
+
 //* ----- ROUTE DES POST -----
 router.get(`/posts`, postController.getAllPosts);
 router.get(`/post/:id`, postController.getOnePost);
-router.post(`/post`, postController.addPost);
+router.post(`/post`, upload.single("image"), postController.addPost);
 // router.get(`/${process.env.API_URL}/usersdetails`, userController.getAllUsersDetails);
 // router.get(`/${process.env.API_URL}/userdetails/:id`, userController.getOneUserDetails);
 // router.get(`/${process.env.API_URL}/userdetail/:id`, userController.getOneUserDetail);
@@ -18,7 +21,8 @@ router.delete(`/post/:id`, postController.removePost);
 // router.delete(`/${process.env.API_URL}/user/:id`, userController.deleteUser);
 
 //* ROUTE POUR AVOIR LES TWEET DUN USERID EN FONCTION DE SES LIKES, SAVED RETWEET ETC...
-router.get('/tweetpostuser/:id', postController.getUserPost);
+// router.get('/tweetpostuser/:id', postController.getUserPost);
+router.get('/userpost/:id', postController.getPostByUserId);
 router.get('/savedpostuser/:id', postController.getUserSavedPost);
 router.get('/likedpostuser/:id', postController.getUserLikedPost);
 router.get('/commentedpostuser/:id', postController.getUserCommentedPost);
