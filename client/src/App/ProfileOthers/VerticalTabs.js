f7ef6c37714bf1ca03dad2b7d6fcca08af9d71cd
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-15 16:31:45 +0100

    Create VerticalTabs.js
    Profil ohters done!!

---
+++ client/src/App/ProfileOthers/VerticalTabs.js
@@ -0,0 +1,103 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/require-default-props */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react';
+import {
+  useDispatch,
+  // useSelector,
+} from 'react-redux';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import './styles.css';
+// import classNames from 'classnames';
+import clsx from 'clsx';
+
+// === action
+import { setWhichTweet } from '../../store/action/post';
+
+// eslint-disable-next-line no-unused-vars
+const useStyles = makeStyles((theme) => ({
+  containerMenuItem: {
+  },
+  menuItem: {
+    padding: '0.8em',
+    marginBottom: '0.5em',
+    color: '#828282',
+    fontFamily: 'popins, sans-serif',
+    fontWeight: '600',
+    fontSize: '14px',
+    '&:hover': {
+      cursor: 'pointer',
+      color: '#2F80ED',
+    },
+  },
+}));
+
+const menuItem = [
+  {
+    id: 1,
+    name: 'Tweets',
+    request: 'userpostandcount',
+  },
+  {
+    id: 2,
+    name: 'Tweets & replies',
+    request: 'commentedpostuser',
+  },
+  // {
+  //   id: 3,
+  //   name: 'Medias',
+  //   composant: 'medias',
+  // },
+  {
+    id: 4,
+    name: 'Likes',
+    composant: 'likes',
+    request: 'likedpostuser',
+  },
+];
+
+const VerticalTabs = ({ userSelecteId }) => {
+  // const userId = JSON.parse(localStorage.getItem('userId'));
+
+  const dispatch = useDispatch();
+  const [activeClassId, setActiveClassId] = useState(1);
+  //   const [border, setBorder] = useState('');
+  const classes = useStyles();
+  const handleActiveClass = (menuId, request) => {
+    setActiveClassId(menuId);
+    // const userId = 2;
+    dispatch(setWhichTweet(request, userSelecteId));
+    // console.log('composant', composant);
+  };
+  const verticalJSX = menuItem.map((m) => {
+    let classNameMenuItem = '';
+    if (m.id === activeClassId) {
+      classNameMenuItem += 'activeClassName';
+    }
+    return (
+      <div
+        id="menu-item"
+        key={m.id + 2343}
+        onClick={() => handleActiveClass(m.id, m.request)}
+        className={clsx(classNameMenuItem, classes.menuItem)}
+      >
+        {m.name}
+      </div>
+    );
+  });
+  return (
+    <div className={classes.containerMenuItem}>
+      {verticalJSX}
+      {/* <div className={classes.borderLeft}>Tweets</div> */}
+
+    </div>
+  );
+};
+
+export default VerticalTabs;
+
+VerticalTabs.propTypes = {
+  userSelecteId: PropTypes.number,
+};
